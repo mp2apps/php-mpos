f40a89f0ec62ebfec88881056986f25b4807d830
Sebastian Grewe <sebastian@grewe.ca>
2014-01-11 20:58:52 +0100

    [UPDATE] MPOS version due to major change

--- public/include/version.inc.php
+++ public/include/version.inc.php
@@ -2,7 +2,7 @@
 
 define('DB_VERSION', '0.0.2');
 define('CONFIG_VERSION', '0.0.2');
-define('MPOS_VERSION', '0.0.1');
+define('MPOS_VERSION', '0.0.2');
 
 // Fetch installed database version
 $db_version = $setting->getValue('DB_VERSION');
